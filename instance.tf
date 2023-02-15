b4a0e18d31141513065ee80b0ae0b0430a6d83e1
Justin Reeves <reeves122@gmail.com>
2023-02-15 11:42:19 -0600

    formatting

--- instance.tf
+++ instance.tf
@@ -38,7 +38,7 @@ resource "aws_instance" "this" {
     volume_size           = var.root_block_device_volume_size
     delete_on_termination = true
   }
-  
+
   lifecycle {
     ignore_changes = [
       ami
